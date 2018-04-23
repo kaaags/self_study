1049775147309188c462bd11aa2f11583051e83a
kaaags <seankagawa@gmail.com>
2018-04-22 16:56:45 -1000

    compl FizzBuzz ex & partial ch1

--- automate.py
+++ automate.py
@@ -1,2 +1,14 @@
 testVar = ('does it work?')
-print (testVar)
+print(testVar)
+
+#fizzBuzz
+print('Sample FizzBuzz Exercise')
+for i in range(1,101):
+	if i%3 == 0 and i%5 == 0:
+		print('FizzBuzz')
+	elif i%3 == 0:
+		print('Fizz')
+	elif i%5 == 0:
+		print('Buzz')
+	else:
+		print(i)
---
+++ ch1.py
@@ -0,0 +1,16 @@
+#Ex1
+print(2 + 2)
+
+#Ex2
+print(2)
+
+#Ex3
+print(2 + 3 * 6)
+print((2 + 3) * 6)
+print(48565878 * 578453)
+print(2 ** 8)
+print(23 / 7)
+print(23 // 7)
+print(23 % 7)
+print(2    +           2)
+print((5 - 1) * ((7 + 1) / (3 - 1)))
